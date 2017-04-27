7a6e3c67b1f5395dba2baf8ff4afd33f3a848807
SmithTM90 <smithtm90@gmail.com>
2017-04-27 15:13:35 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var allClear = document.getElementById('allClear');
+  var clearEverything = document.getElementById('clearEverything');
+  var nine = document.getElementById('nine');
+  var eight = document.getElementById('eight');
+  var seven = document.getElementById('seven');
+  var six = document.getElementById('six');
+  var five = document.getElementById('five');
+  var four = document.getElementById('four');
+  var three = document.getElementById('three');
+  var two = document.getElementById('two');
+  var one = document.getElementById('one');
+  var divide = document.getElementById('divide');
+  var multiply = document.getElementById('multiply');
+  var minus = document.getElementById('minus');
+  var plus = document.getElementById('plus');
+  var equals = document.getElementById('equals');
+  var decimal = document.getElementById('decimal');
+
+
+};
